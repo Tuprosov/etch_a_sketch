73d605cc74917dc7ce45dc2bd53fe40ecd9b5408
Shahzod <shahzodwill@gmail.com>
2023-09-14 18:10:51 +0200

    js logic added for basic usage

--- script.js
+++ script.js
@@ -4,13 +4,27 @@ const container = document.querySelector('.container');
 container.appendChild(mainbox)
 
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function changeColor(e) {
+    if(e.type === 'mouseover' && mouseDown){
+        e.target.style.backgroundColor = 'red';
+    } 
+    
+}
+
 
 function createGrid(elem) {
     for (let i = 0; i < 16 * 16; i++) {
         const div = document.createElement('div')
         div.classList.add('square');
+        div.addEventListener('mousedown', changeColor)
+        div.addEventListener('mouseover', changeColor)
         elem.appendChild(div);
     }
 }
 
 createGrid(mainbox);
+
